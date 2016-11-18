67f2253aa1f50e5a66144f647bd71e9af3348bdd
Dion Dokter <diondokter@gmail.com>
2016-11-18 14:25:12 +0100

    First Xamarin Setup

---
+++ App/Tscrump App/Tscrump App/Tscrump_App/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Tscrump_App
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new Tscrump_App.MainPage();
+		}
+
+		protected override void OnStart()
+		{
+			
+		}
+
+		protected override void OnSleep()
+		{
+			
+		}
+
+		protected override void OnResume()
+		{
+			
+		}
+	}
+}
