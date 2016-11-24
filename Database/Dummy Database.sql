2b928a7386caaa8b6613939e6e42ec532dc76c4c
Faalhaaz <accvoorwarrock@gmail.com>
2016-11-24 14:19:17 +0100

    Update Dummy Database.sql

--- Database/Dummy Database.sql
+++ Database/Dummy Database.sql
@@ -1,5 +1,7 @@
-CREATE DATABASE  IF NOT EXISTS `dummyweatherstation` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `dummyweatherstation`;
+DROP DATABASE weatherr;
+
+CREATE DATABASE  IF NOT EXISTS `weatherr` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `weatherr`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dummyweatherstation
@@ -21,10 +23,10 @@ USE `dummyweatherstation`;
 -- Table structure for table `dummysensorvalues`
 --
 
-DROP TABLE IF EXISTS `dummysensorvalues`;
+DROP TABLE IF EXISTS `sensor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dummysensorvalues` (
+CREATE TABLE `sensor` (
   `Date` datetime NOT NULL,
   `Temperature` float DEFAULT NULL,
   `Pressure` float DEFAULT NULL,
@@ -38,9 +40,9 @@ CREATE TABLE `dummysensorvalues` (
 -- Dumping data for table `dummysensorvalues`
 --
 
-LOCK TABLES `dummysensorvalues` WRITE;
+LOCK TABLES `sensor` WRITE;
 /*!40000 ALTER TABLE `dummysensorvalues` DISABLE KEYS */;
-INSERT INTO `dummysensorvalues` VALUES ('2016-11-16 18:00:00',20,1.005,0.2,0.6),('2016-11-17 18:00:00',21,0.998,0.5,0.6),('2016-11-17 19:00:00',19,0.986,0.4,0.5),('2016-11-17 20:00:00',18,0.975,0.33,0.4),('2016-11-17 21:00:00',15,0.983,0.2556,0.3);
+INSERT INTO `sensor` VALUES ('2016-11-16 18:00:00',20,1.005,0.2,0.6),('2016-11-17 18:00:00',21,0.998,0.5,0.6),('2016-11-17 19:00:00',19,0.986,0.4,0.5),('2016-11-17 20:00:00',18,0.975,0.33,0.4),('2016-11-17 21:00:00',15,0.983,0.2556,0.3);
 /*!40000 ALTER TABLE `dummysensorvalues` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
