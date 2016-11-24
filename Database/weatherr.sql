3bceece397d837604d4b772dfb4606b697ab65f1
Faalhaaz <accvoorwarrock@gmail.com>
2016-11-24 14:07:13 +0100

    Update weatherr.sql

--- Database/weatherr.sql
+++ Database/weatherr.sql
@@ -1,24 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Gegenereerd op: 24 nov 2016 om 13:58
--- Serverversie: 10.1.19-MariaDB
--- PHP-versie: 5.6.24
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `weatherr`
---
+CREATE DATABASE IF NOT EXISTS `weatherr` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `weatherr`;
 
 -- --------------------------------------------------------
 
