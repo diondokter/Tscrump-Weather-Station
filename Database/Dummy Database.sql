2ce8b78ef9acfe06e545d4c2f8f4c2049d7fd1c1
Faalhaaz <accvoorwarrock@gmail.com>
2016-11-24 14:23:15 +0100

    Update Dummy Database.sql

--- Database/Dummy Database.sql
+++ Database/Dummy Database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE  IF NOT EXISTS `weatherr` /*!40100 DEFAULT CHARACTER SET utf8 */
 USE `weatherr`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: localhost    Database: dummyweatherstation
+-- Host: localhost    Database: weatherr
 -- ------------------------------------------------------
 -- Server version	5.6.32-log
 
@@ -20,7 +20,7 @@ USE `weatherr`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `dummysensorvalues`
+-- Table structure for table `sensor`
 --
 
 DROP TABLE IF EXISTS `sensor`;
@@ -37,13 +37,13 @@ CREATE TABLE `sensor` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `dummysensorvalues`
+-- Dumping data for table `sensor`
 --
 
 LOCK TABLES `sensor` WRITE;
-/*!40000 ALTER TABLE `dummysensorvalues` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sensor` DISABLE KEYS */;
 INSERT INTO `sensor` VALUES ('2016-11-16 18:00:00',20,1.005,0.2,0.6),('2016-11-17 18:00:00',21,0.998,0.5,0.6),('2016-11-17 19:00:00',19,0.986,0.4,0.5),('2016-11-17 20:00:00',18,0.975,0.33,0.4),('2016-11-17 21:00:00',15,0.983,0.2556,0.3);
-/*!40000 ALTER TABLE `dummysensorvalues` ENABLE KEYS */;
+/*!40000 ALTER TABLE `sensor` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
