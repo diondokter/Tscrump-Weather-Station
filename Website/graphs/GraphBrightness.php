ab71f4837ecc66ae65f9f19fd2c3d5198ac444bf
Faalhaaz <accvoorwarrock@gmail.com>
2017-01-16 12:20:57 +0100

    Update GraphBrightness.php

--- Website/graphs/GraphBrightness.php
+++ Website/graphs/GraphBrightness.php
@@ -16,8 +16,8 @@ if ($conn->connect_error) {
 /* Creates variables for usage */
 $y_axis = array();
 $x_axis = array();
-$index = 0;
-$sql = "SELECT * FROM sensor group by Date limit 24";
+$index = 24;
+$sql = "SELECT * FROM sensor group by Date desc limit 24";
 /* Gets the result from the SQL Query, when none is given it triggers an error */
 $result = $conn->query($sql) or trigger_error($mysqli->error."[$sql]");
 
@@ -25,7 +25,7 @@ $result = $conn->query($sql) or trigger_error($mysqli->error."[$sql]");
 while($row = mysqli_fetch_array($result)) {
 $y_axis[$index] = $row["Brightness"]; // Gives the y_axis the Temperatures
 $x_axis[$index] = $row["Date"]; // Gives the x_axis the Time
-    $index++;
+    $index--;
 }
 
 /* Graph Settings */
