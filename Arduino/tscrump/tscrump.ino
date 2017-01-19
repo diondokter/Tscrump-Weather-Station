b20f919725ee0632757a160d0ff88a31cf34f759
Emre Elmas <elmas.emre@gmail.com>
2017-01-19 20:56:51 +0100

    Working version

--- Arduino/tscrump/tscrump.ino
+++ Arduino/tscrump/tscrump.ino
@@ -2,10 +2,12 @@
 #include <Adafruit_BMP085_U.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <rn2xx3.h>
+
 
 // Set your AppEUI and AppKey
-const char *appEui = "70B3D57EF000335E";
-const char *appKey = "CC745AC6D0373813AA14466BE367D301";
+const char *appEui = "70B3D57ED00016FA";
+const char *appKey = "D2800EA3EE7DA994252AEE2944B0A093";
 
 #define LightPin A0
 #define loraSerial Serial
@@ -15,78 +17,90 @@ const char *appKey = "CC745AC6D0373813AA14466BE367D301";
 #define DHTPIN 4
 #define DHTTYPE DHT11
 #define SENSORS 5
+
 DHT_Unified dht(DHTPIN, DHTTYPE);
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(55);
+rn2xx3 myLora(loraSerial);
 
 
 void setup() {
   loraSerial.begin(57600);
-  debugSerial.begin(115200);
-  delay(10000);
-  loraSerial.println("mac set appkey D2800EA3EE7DA994252AEE2944B0A093");
-  delay(100);
-  loraSerial.println("mac set appeui 70B3D57ED00016FA");
-  delay(100);
-  loraSerial.println("mac join otaa");
+  debugSerial.begin(57600);
   delay(5000);
   while (loraSerial.available()) {
     debugSerial.write(loraSerial.read());
   }
-  // Wait a maximum of 10s for Serial Monitor
-  while (!debugSerial && millis() < 10000);
+  while ((!loraSerial) && (millis() < 10000));
+  initialize_radio();
   pinMode(LightPin, INPUT);
-
   if (!bmp.begin()) {
     debugSerial.println("BMP180 not working");
   }
   sensor_t humidity;
   sensor_t temperature;
-
   dht.humidity().getSensor(&humidity);
   dht.temperature().getSensor(&temperature);
+  myLora.tx("TTN Mapper on TTN Custom PCB with SAMD21G18 and RN2483");
 }
 
 void loop() {
-  int tmpSensorArray[5];
-  uint16_t light = analogRead(LightPin);
-  
+  float tmpSensorArray[SENSORS];
+  tmpSensorArray[4] = analogRead(LightPin);
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     debugSerial.println("Error reading temperature!");
-  } 
-  tmpSensorArray[0] = (int)event.temperature;
-  
+  }
+  tmpSensorArray[0] = event.temperature;
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     debugSerial.println("Error reading humidity!");
   }
-  tmpSensorArray[3] = (int)event.relative_humidity;  
-  
+  tmpSensorArray[3] = event.relative_humidity;
+
   // Get humidity event and print its value.
   bmp.getEvent(&event);
   if (isnan(event.pressure)) {
     debugSerial.println("Error reading humidity!");
   }
-  tmpSensorArray[2] = (int)event.pressure;
-  
-  
+  tmpSensorArray[2] = event.pressure;
   float bmpTemperature;
   bmp.getTemperature(&bmpTemperature);
-
-  tmpSensorArray[1] = (int)bmpTemperature;
+  tmpSensorArray[1] = bmpTemperature;
+  debugSerial.println("TXing");
+  String transmitData = String(tmpSensorArray[0], 2) + "," + String(tmpSensorArray[1], 2) + "," + String(tmpSensorArray[2], 2) + "," + String(tmpSensorArray[3], 2) + "," + String(tmpSensorArray[4], 2);
+  debugSerial.println(transmitData);
+  myLora.tx(transmitData);
   
-  debugSerial.println("Sending LoRa packet");
-  loraSerial.print("mac tx uncnf 123 ");
-  for(int i = 0; i < SENSORS; i++){
-    loraSerial.print((int)(tmpSensorArray[i]));
-    if(i < (SENSORS - 1)){
-      loraSerial.print('A');
-    }
-  }
-  loraSerial.println();
-
-
-
-  delay(20000);
+  delay(60000);
 }
+
+void initialize_radio()
+{
+  delay(100); //wait for the RN2xx3's startup message
+  loraSerial.flush();
+  //print out the HWEUI so that we can register it via ttnctl
+  String hweui = myLora.hweui();
+  while(hweui.length() != 16)
+  {
+    debugSerial.println("Communication with RN2xx3 unsuccessful. Power cycle the board.");
+    delay(10000);
+    hweui = myLora.hweui();
+  }
+  debugSerial.println("When using OTAA, register this DevEUI: ");
+  debugSerial.println(hweui);
+  debugSerial.println("RN2xx3 firmware version:");
+  debugSerial.println(myLora.sysver());
+  debugSerial.println("Trying to join TTN");
+  bool join_result = false;
+  join_result = myLora.initOTAA(appEui, appKey);
+  while(!join_result)
+  {
+    debugSerial.println("Unable to join. Are your keys correct, and do you have TTN coverage?");
+    delay(60000); //delay a minute before retry
+    join_result = myLora.init();
+  }
+  debugSerial.println("Successfully joined TTN");
+
+}
+
